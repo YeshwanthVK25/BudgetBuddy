2d2f9c68da36234a8a32cbce555f4a3e85977b64
YESHWANTHVK25 <vkyeshwanth81@gmail.com>
2026-08-23 17:40:49 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -32,7 +32,7 @@ api.interceptors.response.use(
         const refresh = localStorage.getItem("refresh_token");
 
         const res = await axios.post(
-          `${API_URL}/api/token/refresh/`,
+          `${API_URL}/token/refresh/`,
           {
             refresh,
           }
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -7,7 +7,7 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
 
   const login = async (username, password) => {
-    const res = await api.post("/api/token/", { username, password });
+    const res = await api.post("/token/", { username, password });
     localStorage.setItem("access_token", res.data.access);
     localStorage.setItem("refresh_token", res.data.refresh);
     setUser({ username });
